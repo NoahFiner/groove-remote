b6a848881a3d6c1ac245379169bade8f7ce60ab6
nfiner <nfiner@umich.edu>
2019-03-01 02:06:25 -0500

    Update to version 1.1

--- updater.py
+++ updater.py
@@ -96,7 +96,7 @@ Would you like to update? (y/n):""".format(
 
 
 def special_function():
-    print("Hello from version 1.0!")
+    print("Hello from version 1.1!")
 
 if __name__ == "__main__":
     updater = Updater()
